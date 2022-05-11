8813b232ef0d4943722264c0e76ddabc4b268003
Muhammad Nasif Imtiaz <49706612+muhammadnasif@users.noreply.github.com>
2022-05-11 10:55:43 +0600

    Time: 31 ms (41.33%), Space: 16.8 MB (99.05%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> arr(70005, 0);
+        int val=0;
         for(int i=0;i<nums.size();i++){
-            arr[nums[i]+30000]++;
-        }
-        int val;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==1){
-                val = i-30000;
-            }
+            val = val^nums[i]; 
         }
         return val;
     }
