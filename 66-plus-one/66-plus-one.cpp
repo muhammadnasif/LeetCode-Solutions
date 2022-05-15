f26388a368537dc04d709a820e996a9ff5f6974f
Muhammad Nasif Imtiaz <49706612+muhammadnasif@users.noreply.github.com>
2022-05-15 13:41:22 +0600

    Time: 0 ms (100.00%), Space: 8.6 MB (98.33%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int len = digits.size();
-        int i=len-1;
-        int carry = 0;
-        while(i>=0){
-            // cout<<len<<" "<<digits[i]<<" "<<i<<endl;
-            if(i+1==len){
-                // cout<<"a"<<endl;
-                if(digits[i]+1 == 10){
-                    // cout<<"b"<<endl;
-                    carry = 1;
-                    digits[i] = 0;    
-                }
-                else{
-                    // cout<<"c"<<endl;
-                    carry=0;
-                    digits[i]+=1;
-                }
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;
             }
-            if(carry>0 && i+1!=len){
-                if(digits[i]+carry > 9){
-                    // cout<<"d"<<endl;
-                    digits[i] = digits[i]+carry-10;
-                    carry = 1;
-                }
-                else{
-                    // cout<<"e"<<endl;
-                    digits[i] = digits[i]+carry;
-                    carry=0;
-                }
+            else{
+                digits[i]++;
+                return digits;
             }
-            
-            i--;
         }
-        if(carry >0){
-            digits.insert(digits.begin(), carry);
-        }
+        digits[0]=1;
+        digits.push_back(0);
         return digits;
     }
 };
