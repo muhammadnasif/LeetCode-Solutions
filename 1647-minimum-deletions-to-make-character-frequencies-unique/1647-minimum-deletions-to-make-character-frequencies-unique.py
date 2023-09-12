7d2f125057cd697c45f5c4a7e121e963a1907199
Muhammad Nasif Imtiaz <49706612+muhammadnasif@users.noreply.github.com>
2023-09-12 10:07:44 +0600

    Time: 119 ms (70.49%), Space: 17.1 MB (93.71%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,28 +1,15 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        d = defaultdict(int)
-        chk = defaultdict(bool)
-        
-        for elem in s:
-            d[elem] += 1
-        
-        d = list(d.items())
-        for i in range(len(d)):
-            d[i] = list(d[i])
-
-        chk[d[0][1]] = True   
+        d = Counter(s)
+        chk = defaultdict(bool)        
         res = 0
-        for i in range(1, len(d)):
-            
-            init = d[i][1]
-            cnt  = d[i][1]
-            
+        for k in d:
+            cnt  = d[k]            
             while(chk[cnt] and cnt > 0):
                 cnt -= 1
-
+                res += 1
             chk[cnt] = True
-            d[i][1] = cnt
-            res += (init - cnt)
+            
 
         return res
 
